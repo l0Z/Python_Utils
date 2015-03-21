742d4fd7be57cd0dbccb23eaa091bce4f75dde45
rustle1314 <lili1987mail@gmail.com>
2015-03-21 21:32:41 +0000

    update Logger.py

--- Logger.py
+++ Logger.py
@@ -2,32 +2,57 @@
 import logging;
 import sys;
 
-instance = logging.getLogger("Lili's logger");
+project_name = "lili's logger";
+
+instance = logging.getLogger(project_name);
 instance.setLevel(logging.INFO);
-handler  = logging.StreamHandler(sys.stderr);
+
+handler = logging.StreamHandler(sys.stderr);
 handler.setLevel(logging.INFO);
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s");
+handler.setFormatter(formatter);
+
 instance.addHandler(handler);
 
-def initlog(opts, project):
+def initlog(opts):
     global instance;
     global handler;
-    instance.removeHandler(handler);
-    instance = logging.getLogger(project);
-    handler  = None;
+    global project_name;
 
-    if "log" in opts:
-        handler = logging.FileHandler(opts["log"]);
+    print opts;
+    if "project_name" in opts:
+        project_name = opts["project_name"];
+        print "in Logger", project_name;
+
+    instance.removeHandler(handler);
+    instance = logging.getLogger(project_name);
+
+    #set longer
+    if "logfile" in opts:
+        handler = logging.FileHandler(opts["logfile"]);
     else:
         handler = logging.StreamHandler(sys.stderr);
+    
+    #set formatter
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s");
+    handler.setFormatter(formatter);
 
+    ##set level
     instance.setLevel(logging.INFO);
     if "level" in opts:
-        if   "debug" == opts["level"].lowcase():
-            instance.setLevel(logging.DEBUG);
+        if "notset" == opts["level"].lowcase():
+            instance.setLevel(logging.NOTSET)
+        elif "debug" == opts["level"].lowcase():
+            instance.setLevel(logging.DEBUG)
         elif "info"  == opts["level"].lowcase():
-            instance.setLevel(logging.INFO);
-        else:
-            instance.setLevel(logging.INFO);
-    
+            instance.setLevel(logging.INFO)
+        elif "warning" == opts["level"].lowcase():
+            instance.setLevel(logging.WARNING)
+        elif "error" == opts["level"].lowcase():
+            instance.setLevel(logging.ERROR)
+        elif "critical" == opts["level"].lowcase():
+            instance.setLevel(logging.critical)   
+
     instance.addHandler(handler);
    
